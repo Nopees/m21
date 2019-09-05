b5e6ade016bd9620887290941b9e88aadb86d8f6
Nopees <45964918+Nopees@users.noreply.github.com>
2019-09-05 11:10:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("618465530436648982")
+let channel =     client.channels.get("619084017739890691")
 setInterval(function() {
 channel.send(`haroonylove`);
 }, 30)
